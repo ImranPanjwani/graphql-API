6023c4500d773962e6ed3dcb1eae795bf6efa2be
Imran Panjwani <imranpanjwani@Imrans-MacBook-Pro.local>
2018-09-23 16:47:25 +0530

    db and environment setup

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
+import 'dotenv/config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
-import models from './models';
 import resolvers from './resolvers';
 import schema from './schema';
+import models, { sequelize } from './models';
 
 const app = express();
 
 const server = new ApolloServer({
-    typeDefs: schema,
-    resolvers,
-    context: {
-        models,
-        me : models.users[0]
-    }
+  typeDefs: schema,
+  resolvers,
+  context: {
+    // models
+    // me : models.users[0]
+  }
 });
 
-server.applyMiddleware({ app, path: '/graphql'});
+server.applyMiddleware({ app, path: '/graphql' });
 
-app.listen({ port : 8000 }, () => {
-    console.log('Apollo server at: http://localhost:8000/graphql');
+sequelize.sync().then(async () => {
+  app.listen({ port: 8000 }, () => {
+    console.log('Apollo Server on http://localhost:8000/graphql');
+  });
 });
---
+++ src/models/message.js
@@ -0,0 +1,15 @@
+const message = (sequelize, DataType) => {
+    const Message = sequelize.define('message', {
+        text: {
+            type: DataType.STRING
+        }
+    });
+
+    Message.associate = models => {
+        Message.belongsTo(models.User);
+    }
+
+    return Message;
+}
+
+export default message;
